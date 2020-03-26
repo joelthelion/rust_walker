e739172b32fc377f676228c2a345ed8401d7066f
Joël Schaerer <joel.schaerer@laposte.net>
2020-03-27 00:56:44 +0100

    basic structure in place

--- .vimrc
+++ .vimrc
@@ -1,3 +1,4 @@
+let $RUST_BACKTRACE=1
 map !! iprintln!(
 set makeprg=cargo\ run\ --bin\ rust_walker
 set wildignore+=target
--- src/rust_walker.rs
+++ src/rust_walker.rs
@@ -4,77 +4,83 @@ use rand::thread_rng;
 use rand::Rng;
 use std::env;
 // use std::option::Option;
+use futures::future::{Future, FutureExt};
+use futures::stream::futures_unordered::FuturesUnordered;
+use futures::stream::{Stream, StreamExt};
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::vec::Vec;
-use indexmap::IndexMap;
-use futures::stream::{Stream, StreamExt};
-use futures::future::{FutureExt, Future};
-use futures::stream::futures_unordered::FuturesUnordered;
 use tokio::task::JoinHandle;
-use NodeType::*;
 
-type ChildrenType = IndexMap<PathBuf, NodeType>;
-type CrawlResultType = tokio::io::Result<ChildrenType>;
+type ChildrenType = Vec<PathBuf>;
+type CrawlResultType = tokio::io::Result<(PathBuf, ChildrenType)>;
 
 #[derive(Debug)]
 enum NodeType {
-    Nothing,
     Pending,
-    Full(DirNode),
-}
-
-#[derive(Debug)]
-struct DirNode {
-    children: ChildrenType
+    Empty,
+    Full(ChildrenType),
 }
 
 async fn get_children(path: PathBuf) -> CrawlResultType {
-    let mut children = IndexMap::new();
-    let mut entries = tokio::fs::read_dir(path).await?;
-    while let Some(i_) =  entries.next().await {
+    let mut children = Vec::new();
+    let mut entries = tokio::fs::read_dir(&path).await?;
+    while let Some(i_) = entries.next().await {
         let i = i_?;
         let is_symlink = i.file_type().await?.is_symlink();
         let path = i.path();
         if !is_symlink && path.is_dir() {
-            children.insert(path, NodeType::Nothing);
+            children.push(path);
         } else {
             println!("{}", path.display());
         }
     }
-    Ok(children)
+    Ok((path, children))
 }
 
-
 async fn random_walk(path_: &str) {
-    let mut task_queue : FuturesUnordered<JoinHandle<CrawlResultType>> = FuturesUnordered::new();
-    let mut current = NodeType::Nothing;
-    let path: PathBuf = PathBuf::from(path_);
+    let mut task_queue: FuturesUnordered<JoinHandle<CrawlResultType>> = FuturesUnordered::new();
+    let mut nodes: HashMap<PathBuf, NodeType> = HashMap::new();
     loop {
-        match &mut current {
-            Nothing => {
-                // spawn
-                let task = tokio::spawn(get_children(path.clone()));
-                task_queue.push(task);
-                current = NodeType::Pending;
-            }
-            Pending => { break; }
-            Full(node) => {
-                // descend
-                let child_idx;
-                {
-                    let mut rng = thread_rng();
-                    child_idx = rng.gen_range(0, node.children.len());
-                    let (path, current) = node.children.get_index_mut(child_idx).unwrap();
+        let mut path: PathBuf = PathBuf::from(path_);
+        loop {
+            match nodes.get(&path) {
+                None => {
+                    // spawn
+                    let task = tokio::spawn(get_children(path.clone()));
+                    task_queue.push(task);
+                    nodes.insert(path.clone(), NodeType::Pending);
+                }
+                Some(NodeType::Pending) => {
+                    break;
+                }
+                Some(NodeType::Empty) => {
+                    break;
+                } // FIXME shouldn't get here
+                Some(NodeType::Full(children)) => {
+                    // descend
+                    let child_idx;
+                    {
+                        let mut rng = thread_rng();
+                        child_idx = rng.gen_range(0, children.len());
+                        path = children[child_idx].clone();
+                    }
                 }
             }
         }
-    }
-    if let Some(Ok(children)) = task_queue.next().await {
-        println!("{:?}", children);
+        if let Some(Ok(Ok((path, children)))) = task_queue.next().await {
+            // Update entry
+            if children.len() == 0 {
+                nodes.insert(path, NodeType::Empty);
+            } else {
+                nodes.insert(path, NodeType::Full(children));
+            }
+        } else {
+            // If nothing in queue. FIXME handle crawling errors
+            break;
+        }
     }
 }
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
